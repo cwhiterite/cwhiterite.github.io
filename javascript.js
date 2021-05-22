44ed0c3fd126edcf109b86745fcc3820b1e9fd8e
cwhiterite <cwhiterite@gmail.com>
2021-05-22 09:43:54 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,34 +1,4 @@
 
-var elem = document.getElementById("body");
-
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-function lock(orientation) {
-  // Go into full screen first
-  if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
-
-  // Then lock orientation
-  screen.orientation.lock(orientation);
-}
-
-
-
 function put_marker(from_left, from_top, floor) {
     with(document.getElementById('marker')) {
         style.left = from_left + "px";
