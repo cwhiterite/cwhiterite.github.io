71941ef6c073454b553288f27856210c1ec3686b
cwhiterite <cwhiterite@gmail.com>
2021-05-22 09:28:54 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,23 @@
+
+
+function lock(orientation) {
+  // Go into full screen first
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+
+  // Then lock orientation
+  screen.orientation.lock(orientation);
+}
+
+
+
 function put_marker(from_left, from_top, floor) {
     with(document.getElementById('marker')) {
         style.left = from_left + "px";
